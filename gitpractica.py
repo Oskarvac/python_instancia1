e750652958e937e927e6882ec5cff523534728bc
Oskar Vac <oskarvac@gmail.com>
2025-10-30 07:46:52 -0500

    Se creo la funcion sumar

---
+++ gitpractica.py
@@ -0,0 +1,8 @@
+print("Este es un espacio de practica con Git")
+
+def sumar(n1, n2):
+    resultado = n1 + n2
+    print("El resultado de la suma es:", resultado)
+    return resultado
+
+sumar(5, 7)
